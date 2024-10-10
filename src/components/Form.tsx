317f8f9dbb8e0d71448cabcf8959b28fb9f6d92b
Dana Gamez <danagamezdbg@gmail.com>
2024-10-09 21:18:57 -0600

    Add Activity reducer para obtener los datos de los campos de calories, activities y del select.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,8 @@
 import { categories } from "../data/db"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 import { Activity } from "../types/indx"
+
+
 
 const Form = () => {
     const [activity, setActivity]=useState<Activity>({
@@ -20,9 +22,14 @@ const Form = () => {
         return name.trim()!=='' && calories>0
     }
 
+    const onSubmit=(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+    }
     return (
-      <form className="space-y-5 bg-white shadow p-10 rounded-lg">
-
+      <form className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={onSubmit}
+      >
+            
            <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoria:</label>
             <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white" value={activity.category} onChange={handleSubmit}>
@@ -38,7 +45,6 @@ const Form = () => {
         <label htmlFor="calories" className="font-bold">Calorias: </label>
         <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. 300 o 500" value={activity.calories} onChange={handleSubmit}/>
     </div>
-
     <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10" value={activity.category===1?'Guardar Comida':'Guardar Ejercicio'} disabled={!isValidActivity()}/>
        </form>
   )
---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,18 @@
+import { Activity } from "../types/indx"
+
+export type ActivityActions={
+
+}
+
+type ActivityState={
+    activities:Activity[]
+}
+export const initialState={
+
+
+}
+
+
+export const actvityReducer=()=>{
+
+}

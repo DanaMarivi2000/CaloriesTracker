ed13ee1802369ed47f56531c7c75e5c309985bdb
Dana Gamez <danagamezdbg@gmail.com>
2024-10-14 13:35:48 -0600

    'Add activities validation'

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -11,12 +11,15 @@ type ActivityListProps={
 const ActivityList = ({activities, dispatch}:ActivityListProps) => {
   
   const categoryName=useMemo(()=>(category:Activity['category'])=>categories.map(categoria=>categoria.id===category?categoria.name:''),[activities])
-    return (
+ const isEmptyActivities=useMemo(()=>activities.length===0,[activities])
+
+  return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
         Comida y Actividades
       </h2>
-      {activities.map(activity=>(
+      {isEmptyActivities?<p className="text-center my-5">No hay actividades aún</p>:
+      activities.map(activity=>(
         <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
             <div className="space-y-2 relative">
                 <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category===1 ?'bg-lime-500':'bg-orange-500'}`}>
